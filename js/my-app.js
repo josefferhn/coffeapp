b0f7b53e6e59a8be6201de6034e7aa1209339ba5
josefferhn <josefferhn@gmail.com>
2020-09-17 18:39:16 -0600

    actualización final

--- js/my-app.js
+++ js/my-app.js
@@ -51,6 +51,20 @@ $$('#btnConfirmar').on('click', function () {
 });
 
 
+$$('#btnLogin').on('click', function () {
+	var Usuario = $$('#username').val();
+	var Password = $$('#password').val();
+	
+  	if(Usuario == "jframos" && Password == "123"){
+		app.dialog.alert('¡ Bienvenido a Coffee Shop !');
+		app.loginScreen.close(".modal-login");
+		
+	}else{
+		app.dialog.alert('Lo sentimos, sus datos son incorrectos');
+	}
+	
+	
+});
 
 
 
